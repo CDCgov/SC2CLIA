0c8231ebb2f720c69ec5f2f6edf813690be79409
Jin <qtl7@cdc.gov>
2021-03-20 15:01:56 -0400

    merge scripts to nextflow workflow

--- run_cecret.sh
+++ run_cecret.sh
@@ -38,11 +38,13 @@ OUTDIR=$PWD/Run_$current_time
 
 $CECRET_BASE/nextflow run $CECRET_NEXTFLOW -c $CONFIG --reads $DATA --outdir $OUTDIR --kraken2 true --kraken2_db=$CECRET_BASE/kraken2_db
 
+# -- the following scripts are moved to nextflow workflow instead --
+
 # this file might be confusing, it is the same as the 'summary.txt' under each Run folder
-rm run_results.txt
+#rm run_results.txt
 
 # parse the vcf files and add len_largest_deletion, len_largest_insertion to the result file
-python3 vcf_parser.py -d $OUTDIR/bcftools_variants -o $OUTDIR/summary.txt
+#python3 vcf_parser.py -d $OUTDIR/bcftools_variants -o $OUTDIR/summary.txt
 
 # parse the ampliconstats.txt files and add create a folder to hold amplicon dropout info
-python3 amplicon_stat.py -d $OUTDIR/samtools_ampliconstats -o $OUTDIR/amplicon_dropout_summary
+#python3 amplicon_stat.py -d $OUTDIR/samtools_ampliconstats -o $OUTDIR/amplicon_dropout_summary
