7e010e2eee4f2b73e17c1fb75297d871ab5725b3
Thakur <pei6@monolith3.biotech.cdc.gov>
2021-04-19 10:30:37 -0400

    Some fixes to ncbi upload

--- Cecret/make_src.py
+++ Cecret/make_src.py
@@ -22,27 +22,19 @@ import ELIMS_metadata_ingest_gw_mo as gwmo
 # 3002351198-N8KCQD6Z
 # 3002351199-N8KCQD74
 
-# with open("sample.txt", 'r') as openfile:
-    # ID_DICT = dict()
-    # for line in openfile:
-        # split line [CSID, ARTIFACTID]
-            #ID_DICT = {CSID: ARTIFACTID}
-
-# csids=[map str() to ID_DICT.key()]
-#for csid in csids:
-    # valid_csids+=csid.checked()
 
 
-csid=[str(3002228910), str(3002228998)]
-test_dict=gwmo.get_dict_NCBI_UPLOAD(csid)
+# csid=[str(3002228910), str(3002228998)]
+# test_dict=gwmo.get_dict_NCBI_UPLOAD(csid)
 
 
 
 # test_dict = {'3031322614': {'Organism': 'Severe acute respiratory syndrome coronavirus 2', 'host': 'Homo sapien', 'collection-date': '11/03/2020', 'isolation-source': 'clinical', 'country': 'USA: KS'}, '3031324393': {'Organism': 'Severe acute respiratory syndrome coronavirus 2', 'host': 'Homo sapien', 'collection-date': '01/20/2021', 'isolation-source': 'clinical', 'country': 'USA: LA'}}
 def print_header():
-    print("sequence_ID\torganism\thost\tcollection_date\tisolation_source\tcountry")
-    return "sequence_ID\torganism\thost\tcollection_date\tisolation_source\tcountry"
-def print_row(key, val):
+    print("sequence_ID\torganism\thost\tcollection_date\tisolation_source\tcountry\tisolate")
+    return "sequence_ID\torganism\thost\tcollection_date\tisolation_source\tcountry\tisolate"
+
+def print_row(key, val, id_dict):
     sequence_ID = key
     Organism = val["Organism"]
     Host = val["host"]
@@ -50,44 +42,71 @@ def print_row(key, val):
     collection_date = fix_collection_date(collection_date)
     Isolation_Source = val["isolation-source"]
     country = val["country"]
+    isolate = "TEST"
     if validate_sequenceid(sequence_ID) and validate_organism(Organism) and validate_host(Host) and validate_date(collection_date):
-        # print("Success!")
-        print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(sequence_ID, Organism, Host, collection_date, Isolation_Source, country))
-        return "{0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(sequence_ID, Organism, Host, collection_date, Isolation_Source, country)
+        # cuid = id_dict[key]
+        print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}".format(id_dict[key], Organism, Host, collection_date, Isolation_Source, country, isolate))
+        return "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}".format(id_dict[key], Organism, Host, collection_date, Isolation_Source, country, isolate)
     else:
         print("Failure")
+
 def validate_sequenceid(sequence_id):
     if re.search('\d{10}|\w{8}', sequence_id):
         return True
     else:
         return False
+
 def validate_organism(Organism):
     if Organism == "Severe acute respiratory syndrome coronavirus 2":
         return True
     else:
         return False
+
 def validate_host(Host):
     if Host == "Homo sapien":
         return True
     else:
         return False
+
 def validate_date(collection_date):
     if re.search('\d{4}-\d{2}-\d{2}', collection_date):
         return True
     else:
         return False
+
 def validate_isolationsource(Isolation_Source):
     pass
+
 def fix_collection_date(collection_date):
     date_list = collection_date.split("/")
     date_list.reverse()
     return "-".join(date_list)
+
+def validate_csid(csid):
+    return re.search('\d{10}', csid)
+
 if __name__ == "__main__":
+
+    csid_list = []
+    cuid_list = []
+    ID_DICT = dict()
+    with open("samples.txt", 'r') as openfile:
+        for line in openfile:
+            line = line.rstrip("\n")
+            ids = line.split("\t")
+            csid = ids[0]
+            cuid = ids[1]
+            if validate_csid(csid):
+                ID_DICT[csid] = cuid
+                csid_list.append(str(csid))
+                cuid_list.append(str(cuid))
+
+    test_dict=gwmo.get_dict_NCBI_UPLOAD(csid_list)
+
     with open("output.csv", 'w') as f:
         w = csv.writer(f, delimiter='\t')
         header = print_header()
         w.writerow(header)
         for key, val in test_dict.items():
-            row = print_row(key, val)
-            # print(key, val)
+            row = print_row(key, val, ID_DICT)
             w.writerow(row)
