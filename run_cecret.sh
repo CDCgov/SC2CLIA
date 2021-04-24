3704566402df4c2947a49b75c6413a66fa85a886
Thakur <pei6@monolith3.biotech.cdc.gov>
2021-04-24 04:28:58 -0400

    Changed run_cecret-sh to almost work with R script

--- run_cecret.sh
+++ run_cecret.sh
@@ -40,7 +40,7 @@ CONFIG=$PWD/Cecret/configs/singularity.config
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 #OUTDIR=$CECRET_BASE/Run_$current_time
-OUTDIR=$PWD/Run\_$current_time\_$(basename $DATA)
+OUTDIR=$PWD/Run_${current_time}_$(basename $DATA)
 
 $CECRET_BASE/nextflow run $CECRET_NEXTFLOW -c $CONFIG --reads $DATA --outdir $OUTDIR \
 							--kraken2 true --kraken2_db=$CECRET_BASE/kraken2_db \
@@ -49,36 +49,25 @@ $CECRET_BASE/nextflow run $CECRET_NEXTFLOW -c $CONFIG --reads $DATA --outdir $OU
 # Stops the ^H character from being printed after running Nextflow
 stty erase ^H
 
-# -- the following scripts are moved to nextflow workflow instead --
 
-# this file might be confusing, it is the same as the 'summary.txt' under each Run folder
-#rm run_results.txt
+# Begin Report block
+echo "running R scripts to generate reports ..."
 
-# parse the vcf files and add len_largest_deletion, len_largest_insertion to the result file
-#python3 vcf_parser.py -d $OUTDIR/bcftools_variants -o $OUTDIR/summary.txt
-
-# parse the ampliconstats.txt files and add create a folder to hold amplicon dropout info
-#python3 amplicon_stat.py -d $OUTDIR/samtools_ampliconstats -o $OUTDIR/amplicon_dropout_summary
-
-echo running R scripts to generate reports ...
-
-SC2-Seq-CLIA_mnt= ***replace with your own path here***
 R_IMG=$CECRET_BASE/SINGULARITY_CACHE/singularity-r.sif
-R_script=$CECRET_BASE/Cecret/bin/report/config.R
+R_folder=${PWD}/Cecret/bin/report
 
+# -r, -a, and -s
+runID=$(basename $DATA)
+analysisDir=$OUTDIR
+seqDir=$(realpath $DATA)
 
-cd Cecret/bin/report
+##singularity exec --no-home -B  ***replace with your own path here***
 
-# singularity exec -B  ***replace with your own path here***
-# 				-a /mnt/Run\_$current_time\_$(basename $DATA) \
-# 				-r $(basename $DATA) \
-# 				-s /mnt/runs > /dev/null 
+singularity exec \
+				--no-home \
+				-B $seqDir:/data:ro,${R_folder}:/usr/local/bin:rw,${analysisDir}:/OUTDIR:rw \
+				-H /usr/local/bin \
+				${R_IMG} \
+					config.R -r $runID -a /OUTDIR -s /data > /dev/null
 
-singularity exec -B $SC2-Seq-CLIA_mnt:/mnt $R_IMG Rscript $R_script \
-				 -a /mnt/cecret_test_prod/Run\_$current_time\_$(basename $DATA) \
-				 -r $(basename $DATA) \
-				 -s $SC2-Seq-CLIA_mnt/runs/$(basename $DATA) > /dev/null
-				 # note it will fail for run folder like this: 
-				 #  ***replace with your own path here***
-
-echo Done !
+echo "Done!"
